b8cc64e55c5391ac3955e19e2b52410d572b62b5
Pablo Sanchez <psanchezdev345@gmail.com>
2023-08-07 14:46:57 -0600

    connected main css and js files

--- includes/cloud-chatter-scripts.php
+++ includes/cloud-chatter-scripts.php
@@ -1,3 +1,8 @@
 <?php
 
 //Load JS on the front end
+function cloud_chatter_scripts(){
+  wp_enqueue_script( 'cloud-chatter-frontend-scripts', CLOUD_CHATTER_URL . '/js/cloud-chatter-frontend-scripts.js', [], time(), true );
+}
+
+add_action( 'wp_enqueue_scripts', 'cloud_chatter_scripts' );
--- includes/cloud-chatter-styles.php
+++ includes/cloud-chatter-styles.php
@@ -1,3 +1,8 @@
 <?php
 
 //Load Styles
+function cloud_chatter_styles(){
+  wp_enqueue_style( 'cloud-chatter-frontend-styles', CLOUD_CHATTER_URL , '/css/cloud-chatter-frontend-styles.css', [], time(), 'all' );
+}
+
+add_action( 'wp_enqueue_scripts', 'cloud_chatter_styles' );
